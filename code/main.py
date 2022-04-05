81c3d4af761e180fdb2127710d7d159121185772
PDLwork <pdlwork@163.com>
2022-04-05 11:06:24 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -68,7 +68,7 @@ def map_update():
     flag = not flag
 
 while True:  # 死循环确保窗口一直显示
-    clock.tick(20)
+    clock.tick(60)
     dragon_jump = False
     for event in pygame.event.get():  # 遍历所有事件
         if event.type == pygame.QUIT:  # 如果程序发现单击关闭窗口按钮
